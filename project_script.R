e69efac9de9a823ea26ab3c62a32332a2fcff700
Zhi Wei Gan <zgan@mit.edu>
2022-05-08 09:04:42 -0400

    WIP CART

--- project_script.R
+++ project_script.R
@@ -35,18 +35,27 @@ head(rowSums(is.na(data_factor_no_fill)))
 colSums(is.na(data_factor_no_fill))
 
 # Make NA's 2's (No)
-data_factor_na_is_no = data.frame(data_factor)
-data_factor_na_is_no[is.na(data_factor)] = "(2) No"
+data_factor_na_is_no = data.frame(data_factor_no_fill)
+data_factor_na_is_no[is.na(data_factor_no_fill)] = "(2) No"
 data_factor_na_is_no
 
+1:ncol(data_factor_no_fill)
+
 # Impute using KNN
-install.packages("DMwR")
-data_factor_knn_impute = 
+# install.packages("abind")
+# install.packages("zoo")
+# install.packages("quantmod")
+# install.packages("xts")
+# install.packages("/Users/zhiweigan/Downloads/DMwR_0.4.1.tar.gz", repos = NULL, type="source")
+library(DMwR)
+data_factor_knn_impute = knnImputation(data_factor_no_fill)
+data_factor_knn_impute
+anyNA(data_factor_knn_impute)
 
 # compute artistic score for several dataframes
-dfList = list(data_factor_no_fill, data_factor_na_is_no)
-dfList <- list(data_factor_no_fill=data_factor_no_fill, 
-               data_factor_na_is_no=data_factor_na_is_no)
+dfList = list(data_factor_no_fill=data_factor_no_fill, 
+              data_factor_na_is_no=data_factor_na_is_no,
+              data_factor_knn_impute=data_factor_knn_impute)
 dfList = lapply(dfList, function(df) {
   df[artistic] = sapply(df[artistic],as.numeric)
   df[artistic] = 1 - (df[artistic] - 1)
@@ -61,30 +70,91 @@ boxplot(dfList$data_factor_na_is_no$ART_SCORE,
         notch = FALSE
 )
 
-hist(dfList$data_factor_na_is_no$ART_SCORE,
-     main = "Artistic Score Histogram",
-     xlab = "Artistic Score")
-hist(dfList$data_factor_no_fill$ART_SCORE,
-     main = "Artistic Score Histogram",
-     xlab = "Artistic Score")
+library(ggplot2)
+ggplot(dfList$data_factor_na_is_no) + 
+  geom_histogram(aes(x = dfList$data_factor_na_is_no$ART_SCORE, y = (..count..)/sum(..count..)), 
+                 bins = 11, fill = "gray", colour = "black") +
+  xlab("Artistic Score") +
+  ylab("Relative Frequency") +
+  ggtitle("Artistic Score NA-is-No Histogram") + 
+  scale_x_continuous(breaks = 0:10)
+ggplot(dfList$data_factor_no_fill) + 
+  geom_histogram(aes(x = dfList$data_factor_no_fill$ART_SCORE, y = (..count..)/sum(..count..)), 
+                 bins = 11, fill = "gray", colour = "black") +
+  xlab("Artistic Score") +
+  ylab("Relative Frequency") +
+  ggtitle("Artistic Score No Fill Histogram") + 
+  scale_x_continuous(breaks = 0:10)
+ggplot(dfList$data_factor_knn_impute) + 
+  geom_histogram(aes(x = dfList$data_factor_knn_impute$ART_SCORE, y = (..count..)/sum(..count..)), 
+                 bins = 11, fill = "gray", colour = "black") +
+  xlab("Artistic Score") +
+  ylab("Relative Frequency") +
+  ggtitle("Artistic Score KNN Impute Histogram") + 
+  scale_x_continuous(breaks = 0:10)
+
+# Split artistic score into several categories so we can do CART and Random Forests
+dfList = lapply(dfList, function(df) {
+  df$ART_INDICATOR <- ifelse(df$ART_SCORE >= 0 & df$ART_SCORE < 1/11, "None",
+                        ifelse(df$ART_SCORE >= 1/11 & df$ART_SCORE < 2/11, "One",
+                           ifelse(df$ART_SCORE >= 2/11 & df$ART_SCORE < 6/11, "Some",
+                              ifelse(df$ART_SCORE >= 6/11, "Many", NA))))
+  df
+})
+
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+
+demographics = c("SURV_LANG",
+                 "GENDER", 
+                 "AGE7", 
+                 "RACETHNICITY",
+                 "EDUC4",
+                 "EMPLOY",
+                 "INCOME",
+                 "REGION4",
+                 "INTERNET")
+
+str(dfList$data_factor_no_fill)
+
+
+CART1 = rpart(ART_INDICATOR ~ SURV_LANG + GENDER + AGE7 + INCOME + INTERNET + EDUC4 + REGION4, 
+              method = "class", 
+              data = dfList$data_factor_no_fill, 
+              control=rpart.control(minsplit=2, minbucket=5, cp=0.003))
+
+printcp(CART1)
+prp(CART1)
+
+CARTPredict1 = predict(CART1, newdata=WikiTest, type="class")
+table(WikiTest$Vandal, CARTPredict1)
 
 # clustering algo
-install.packages('klaR')
+# install.packages('klaR')
 library(klaR)
 num_clusters = 2
 clusters = kmodes(dfList$data_factor_no_fill[demographics], num_clusters, iter.max = 1000, weighted = FALSE, fast = TRUE)
 print("No Fill")
 for(i in 1:num_clusters) {
-  im = mean(dfList$data_factor_no_fill[two_clusters$cluster == i, ]$ART_SCORE, na.rm=TRUE)
+  im = mean(dfList$data_factor_no_fill[clusters$cluster == i, ]$ART_SCORE, na.rm=TRUE)
   print(im)
 }
 
 clusters = kmodes(dfList$data_factor_na_is_no[demographics], num_clusters, iter.max = 1000, weighted = FALSE, fast = TRUE)
 print("NA is No")
 for(i in 1:num_clusters) {
-  im = mean(dfList$data_factor_na_is_no[two_clusters$cluster == i, ]$ART_SCORE, na.rm=TRUE)
+  im = mean(dfList$data_factor_na_is_no[clusters$cluster == i, ]$ART_SCORE, na.rm=TRUE)
   print(im)
 }
+
+clusters = kmodes(dfList$data_factor_knn_impute[demographics], num_clusters, iter.max = 1000, weighted = FALSE, fast = TRUE)
+print("Knn")
+for(i in 1:num_clusters) {
+  im = mean(dfList$data_factor_knn_impute[clusters$cluster == i, ]$ART_SCORE, na.rm=TRUE)
+  print(im)
+}
+
 
 
 
